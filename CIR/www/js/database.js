898ec5faa18476f2935cba352345b6691a1adfe8
mainevillarias <mariecharmaine.villarias@g.msuiit.edu.ph>
2014-03-17 13:11:27 +0800

    fix errors

--- CIR/www/js/database.js
+++ CIR/www/js/database.js
@@ -11,9 +11,7 @@
                     
                 }
                 
-		
                 function createDB(tx){
-		//beverage table
 		 tx.executeSql('CREATE TABLE IF NOT EXISTS DEMO (beverage, caloriePerVolume, calorieVolume)');
                 }
 
@@ -22,48 +20,41 @@
           }
           
           function successCB(){
-            //  alert("Database Installation.. Successfull");
+             // alert("Database Installation Successfull");
              
           }
-    
-		
-		function addBeverage(){
-			db.transaction(addBeverageDB, errorCB);
-			$.mobile.changePage("#beverageList",{reverse:false,transition:"slide"});
-			return false;
-		}
-		
-		function addBeverageDB(tx){
+         function addBeverageDB(tx){
             
-			var _beverage = $("[name='beverageName']").val();
+			var _beverage = $("#beverageName").val();
 
-			var _calorieVolumeUnit = $("[name='calorieVolumeUnit']").val();
-			var _caloriePerVolume = $("[name='caloriePerVolume']").val();
+			var _calorieVolumeUnit = $("#calorieVolumeUnit").val();
+			var _caloriePerVolume = $("#caloriePerVolume").val();
 			
 				if(_calorieVolumeUnit==2){
-					var _calorieVolume = ($("[name='calorieVolume']").val() * 1000);
+					var _calorieVolume = ($("#calorieVolume").val() * 1000);
 				}
 				else if(calorieVolumeUnit==3){
-					var _calorieVolume=($("[name='calorieVolume']").val() * 30);
+					var _calorieVolume=($("#calorieVolume").val() * 30);
 				}
 				else{
-					var _calorieVolume=$("[name='calorieVolume']").val();
+					var _calorieVolume=$("#calorieVolume").val();
 				}
 			
 		      	var sql ='INSERT INTO DEMO (beverage, caloriePerVolume, calorieVolume) VALUES (?,?,?)';
 		      	tx.executeSql(sql,[_beverage, _caloriePerVolume, _calorieVolume],sucessQueryDB,errorCB);
          	}
-
-		function sucessQueryDB(tx){
-              		tx.executeSql('SELECT * FROM DEMO', [], renderList, errorCB);
-              	}
           
-		function renderList(tx,results){
+          function sucessQueryDB(tx){
+             
+              tx.executeSql('SELECT * FROM DEMO', [], renderList, errorCB);
+              
+          }
+          function renderList(tx,results){
               		var htmlstring='';
             		var len = results.rows.length;
             		//alert("Items saved:" +len);
               		for(var i=0;i<len;i++){
-				htmlstring+='<li><a href="#" onClick="inputCalorie(\''+results.rows.item(i).beverage+'\',\''+results.rows.item(i).caloriePerVolume+'\',\''+results.rows.item(i).calorieVolume+'\')"> Name:' + results.rows.item(i).beverage + '<br/> '+results.rows.item(i).caloriePerVolume+' calories per '+results.rows.item(i).calorieVolume+' mL</a></li>'
+				htmlstring+='<li><a href="#" onClick="inputCalorie(\''+results.rows.item(i).beverage+'\',\''+results.rows.item(i).caloriePerVolume+'\',\''+results.rows.item(i).calorieVolume+'\')" > Name:' + results.rows.item(i).beverage + '<br/> '+results.rows.item(i).caloriePerVolume+' calories per '+results.rows.item(i).calorieVolume+' mL</a></li>'
              	 }
 
 			      $('#resultList').html(htmlstring);
@@ -77,51 +68,62 @@
 			return false;
 		}
 
+          
+          
+	//added js
+		function addBeverage(){
+                    db.transaction(addBeverageDB, errorCB);
+                    $.mobile.changePage("#beverageList",{reverse:false,transition:"slide"});
+                    return false;
+                }
+
 		function inputCalorie(beverageName, cPV, cV){
-			var _beverage = document.getElementById("beverage");
+			var _beverage = document.getElementById("beverage2");
 			_beverage.value = beverageName;
 
-			var _cPV = document.getElementById("caloriePerVolume");
+			var _cPV = document.getElementById("caloriePerVolume2");
 			_cPV.value = cPV;
 
-			var _cV = document.getElementById("calorieVolume");
+			var _cV = document.getElementById("calorieVolume2");
 			_cV.value = cV;
 
 			$.mobile.changePage("#inputCalorie",{reverse:false,transition:"slide"});
 			return false;
 		}
 
-		/*function calculateCalories(){
-			db.transaction(calculateCalories2, errorCB);
-			$.mobile.changePage("#totalCalories",{reverse:false,transition:"slide"});
-			return false;
-		}*/
-
 		function calculateCalories2(){
-			_beverageName = $("#beverage").val();
-			_caloriePerVolume = $("#caloriePerVolume").val();
-			_calorieVolume=$("#calorieVolume").val();
+			_beverageName = $("#beverage2").val();
+			_caloriePerVolume = $("#caloriePerVolume2").val();
+			_calorieVolume=$("#calorieVolume2").val();
 			
-			_members = $("#members").val();
-			_quantity = $("#quantity").val();
-			_volumeUnit = $("#volumeUnit").val();
+			_members = $("#members2").val();
+			
+			_quantity = $("#quantity2").val();
+			_volumeUnit = $("#volumeUnitNew").val();
+			
 			
 			if(_volumeUnit==2){
-				_volume=( $("#volume").val() * 1000);
+				_volume=( $("#volume2").val() * 1000);
+
 			}
-			else if(volumeUnit==3){
-				_volume=($("#volume").val()  * 30);
+			else if(_volumeUnit==3){
+				_volume=($("#volume2").val()  * 30);
 			}
 			else{
-				_volume= $("#volume").val();
-				
+				_volume= $("#volume2").val();
 			}
+
+		
+			totalCalories = parseInt( ((_caloriePerVolume/_calorieVolume) * ((_quantity*_volume)/_members)),10 );
+	
+			burntCalorie = parseInt( (totalCalories - ((totalCalories*20)/100)) ,10);
+			water = Math.round(((((totalCalories-burntCalorie)*1000)/33.5)/240));
+			
 
 			var key = totalCalories;
 			var value = burntCalorie;
 			localStorage.setItem(key, value);
 
-			
 			document.getElementById("totalCaloriesResult").innerHTML="<h1>Beverage: </h1>"+_beverageName+"<br/>"+
 										"<h1>Calories Per Volume: </h1>"+_caloriePerVolume+" calories per "+_calorieVolume+" mL"+
 										"<h1>Members: </h1>" +_members+"<br/>"+
@@ -130,14 +132,10 @@
 										"<h1>Total Calories: </h1>"+totalCalories;
 
 			document.getElementById("adviceResult").innerHTML="You can lessen your calorie-intake by 20% by drinking "+water+" glasses of cold water";
-			
+		
 			$.mobile.changePage("#totalCal",{reverse:false,transition:"slide"});
 			return false;
          	}
 
-
-	
-
-	
 
 
